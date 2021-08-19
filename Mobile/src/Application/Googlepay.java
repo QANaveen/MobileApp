dcfccb00a46cdb0b9fa6a3d823ebe89301cd226e
3DiQNaveen <80444474+3DiQNaveen@users.noreply.github.com>
2021-08-19 13:50:45 +0530

    first commit

---
+++ Mobile/src/Application/Googlepay.java
@@ -0,0 +1,11 @@
+package Application;
+
+public class Googlepay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("you can transfer your money using Gpay");
+	}
+
+}
---
+++ Mobile/src/Application/PayTM.java
@@ -0,0 +1,11 @@
+package Application;
+
+public class PayTM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Another app for transfering money");
+	}
+
+}
